279d5910949b26f255829f0a243f177dc3d2277d
luarocks <luarocks@onionmail.org>
2021-05-14 21:35:40 +0300

    up deepCopy

--- src/scriptum/helpers.lua
+++ src/scriptum/helpers.lua
@@ -9,19 +9,6 @@ local help = {}
 --[[ Table ]]--
 
 
-function help.deepCopy(input)
-  if type(input) == "table" then
-    local output = {}
-    for i, o in next, input, nil do
-      output[deepCopy(i)] = deepCopy(o)
-    end
-    return output
-  else
-    return input
-  end
-end
-
-
 --[[ String ]]--
 
 
