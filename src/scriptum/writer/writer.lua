ad0a31ba82ea768cba3b3743e61b88dbd1237afc
luarocks <luarocks@onionmail.org>
2021-05-15 04:25:03 +0300

    add writer

---
+++ src/scriptum/writer/writer.lua
@@ -0,0 +1,19 @@
+--[[
+@title writer
+]]
+
+
+local writer = {}
+
+
+function writer.open(filename)
+  local file = io.open(filename, "w+")
+  if not file then
+    print("error: failed to create '"..filename.."'")
+    return
+  end
+  return file
+end
+
+
+return writer
