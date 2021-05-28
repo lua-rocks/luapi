e4686c437e89b889521429fb915a8841c437aa69
luarocks <luarocks@onionmail.org>
2021-05-28 03:13:03 +0300

    ~

--- src/scriptum/writer/file.lua
+++ src/scriptum/writer/file.lua
@@ -16,17 +16,41 @@ function fileWriter.write(filePath, outPath, module)
   if not file then return end
   local output = {}
 
+  -- search for module table
   for tname, t in pairs(data.tables) do
     if t.order == 1 then
       data.name = tname
-      output.title = t.title
-      output.description = t.description
+      output.text = '# ' .. t.title .. '\n\n' .. t.description .. '\n'
+      break
     end
   end
 
-  file:write('# ' .. output.title .. '\n\n' .. output.description .. '\n\n'
-  )
+  do -- separate internal and external tables and functions
+    output.h2 = {
+      int = {f = {}, t = {}},
+      ext = {f = {}, t = {}}
+    }
+    for tname, t in pairs(data.tables) do
+      t.name = tname
+      if tname:find(data.name) == 1 then
+        output.h2.int.t[t.order] = t
+      else
+        output.h2.ext.t[t.order] = t
+      end
+    end
+    for fname, f in pairs(data.functions) do
+      f.name = fname
+      if fname:find(data.name) == 1 then
+        output.h2.int.f[f.order] = f
+      else
+        output.h2.ext.f[f.order] = f
+      end
+    end
+  end
 
+  dump(output)
+
+  file:write(output.text)
   file:close()
 end
 
